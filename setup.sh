0978b264c7ee1747a9d6b9d018d3062f80782617
Brendon Earl <brendonaearl@gmail.com>
2022-07-16 13:00:48 -0700

    Stop interruption of setup script
    Removes final two lines before running onmyzsh script which open zsh in a subshell
    Sets brew up in shell before calling brew bundle

--- setup.sh
+++ setup.sh
@@ -6,10 +6,13 @@ ssh-add --apple-use-keychain ~/.ssh/id_ed25519
 ln -s .zshrc ~/.zshrc
 
 # Install Oh-My-ZSH
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh | sed '/\s*env\s\s*zsh\s*/d')"
 
 # Install Brew
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+# Prompted configuration (July 2022)
+echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> ~/.zprofile
+eval "$(/opt/homebrew/bin/brew shellenv)"
 
 # Install Mac-CLI
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/guarinogabriel/mac-cli/master/mac-cli/tools/install)"
@@ -31,8 +34,8 @@ open $APPS/Docker.app
 open $APPS/JetBrains\ Toolbox.app
 
 # Cleanup
-brew cleanup --force
-rm -f -r /Library/Caches/Homebrew/*
+brew cleanup
+# rm -f -r /Library/Caches/Homebrew/*
 
 # Other instructions
 echo "Follow https://gist.github.com/kevin-smets/8568070 to setup a nice terminal"
